16153562d48c2a1ccd0e9f49b5b45d85f9d5f46d
Adetoun Olayemi <valenteeena@gmail.com>
2020-11-08 11:09:10 +0100

    Test Project Partial Complete
    Check mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your REST API"/>
+    <test/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    test
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    position: relative;
+    width:90%;
+    margin: auto;
 }
+
 </style>
---
+++ src/components/test.vue
@@ -0,0 +1,118 @@
+<template>
+<div class="table-wrapper">
+    <table id="test-table">
+      <thead>
+        <tr>
+          <th v-for="col in columns" :key="columns[col]">{{col}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in rows" :key="rows[row]">
+                  <td>{{row.project_name}}</td>
+                  <td>{{row.developer}}</td>
+                  <td>{{row.main_contractor}}</td>
+                  <td>{{row.address}}</td>
+                  <td>{{row.state}}</td>
+                  <td>{{row.status}}</td>
+                  <td>{{row.sector}}</td>
+              </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'test',
+  data(){
+    return{
+      columns: ['Project Name','Developer','Main Contractor','Area','State','Status','Sector'],
+      rows:[]
+    }
+  },
+  mounted() {
+        axios
+            .get('https://70c5b72c-65db-4a66-ba01-3e14763157e8.mock.pstmn.io/')
+            .then(response => (
+              this.rows = response.data.data
+            ))
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+$primary-color: #28394b;
+$table-color: #f6fafd;
+$font-family: Arial, sans-serif;
+
+  .table-wrapper{
+    margin: auto;
+    overflow-x: scroll;
+    overflow-y: visible;
+    padding-bottom: 5px;
+    margin-top: 3rem;
+    margin-left: 189px;
+    box-shadow: 0 2px 3px 0 rgba(219,248,227, 0.5), 0 6px 8px 0 rgba(0, 0, 0, 0.19);
+
+    #test-table{
+      text-align: left;
+      border: none;
+      border-spacing: 0;
+      font: normal 12px $font-family;
+      border-radius: .5rem;
+      width:101%;
+      margin: auto;
+
+      th{
+        background-color: $primary-color ;
+        color:white;
+        font-weight: bold;
+        height:3.5rem;
+        border-style: none;
+        padding-left: .5rem;
+        width: auto;
+      }
+
+      td{
+        color: $primary-color;
+        padding-left: .5rem;
+        padding-right: 10px;
+        text-transform: capitalize;        
+        width:auto;
+        height:3.5rem;
+      }
+      td:first-child{
+        font-weight: bold;
+        position: absolute;
+        width:190px;
+        left: 0;
+        top: auto;
+        padding-top: 20px;
+        height:3.6rem;
+      }
+      tr:nth-child(odd) > td:nth-child(odd){
+        background-color: $table-color;
+        width:185px;
+        box-sizing: border-box;
+
+      }
+      th:first-child{
+        position: absolute;
+        width:190px;
+        left:0;
+        border-top-left-radius: .5rem;
+        padding-top: 20px;
+        box-sizing: border-box;
+        height:3.6rem
+      }
+      th:last-child{
+        border-top-right-radius: .5rem;
+      }
+      tr:nth-child(odd){
+      background-color: $table-color;
+      }
+  }
+  }
+
+</style>
